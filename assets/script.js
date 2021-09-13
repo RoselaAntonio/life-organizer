a96bee8154815b868e74b6f13cb3e0652dc35c4a
RoselaAntonio <roselakagamine@gmail.com>
2021-09-12 23:07:32 -0700

    html and js updated

---
+++ assets/script.js
@@ -0,0 +1,20 @@
+//add the function here
+$("document").ready(function()) {
+    //add code for the date on the top
+    $("#currentDay").text(moment().format("dddd, MMMM Do"));
+    //savedTasks
+    var savedTasks = [];
+
+    //Local Storage
+    var checkLS = JSON.parse(localStorage.getItem("schedule"));
+    if (checkLS) {
+        checkLS.forEach(function(entry) {
+            savedTasks.push(entry);
+        });
+        localStorage.setItem("schedule", JSON.stringify(savedTasks));
+    };
+
+    //past, present, future
+    if (moment(time.get))
+
+});
